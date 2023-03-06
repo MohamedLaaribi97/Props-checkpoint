bd18c9392c4e04b3c2ae8263656f72ded475d923
MohamedLaaribi97 <hama.laribi@hotmail.com>
2023-03-06 22:55:27 +0100

    jsxcheckpoint v1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
+import Adress from "./component/profil/Address";
+import Fullname from "./component/profil/FullName";
+import Profilepic from "./component/profil/ProfilePhoto";
 import Mform from "./myform";
+
 function App() {
   return (
     <div className="App">
+      <Profilepic />
+      <div>
+        <Fullname />
+      </div>
+      <div>
+        <Adress />
+      </div>
       <Mform />
     </div>
   );
---
+++ src/component/profil/ProfilePhoto.js
@@ -0,0 +1,6 @@
+import myProfilPicture from "./profilphoto.jpg";
+
+const Profilepic = () => {
+  return <img className="picture" src={myProfilPicture}   alt="Myimage" />;
+};
+export default Profilepic;
