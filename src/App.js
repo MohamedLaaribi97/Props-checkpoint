eb0080c93d984b8ee68be64f61790b78d4b41fb1
MohamedLaaribi97 <hama.laribi@hotmail.com>
2023-03-12 00:05:24 +0100

    propsproject v1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Myprofile from "./Profile/Profile";
+import Myprofile from "./Profile/profile";
 //import biopic from "./Profile/bio.png";
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
         bio="Mohamed is a junior developer with a passion for coding and problem-solving. He recently studiying at GoMyCode coding bootcamp and has a little experience working with HTML, CSS, JavaScript, and React. Mohamed is eager to learn more about web development and is excited to contribute to new projects. In his free time, he enjoys traveling to many destinations, and hitting the gym for a good workout"
         profesion="Web Developer"
         handleName={handleName}
+        
       />
     </>
   );
