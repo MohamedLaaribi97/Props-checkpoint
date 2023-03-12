04173bcbe2b7728bde9b120ad1234c339f0b813f
MohamedLaaribi97 <hama.laribi@hotmail.com>
2023-03-12 23:05:59 +0100

    propsproject v1.3

--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
-const Myprofile = (props,{children}) => {
+const Myprofile = (props, { children }) => {
   return (
     <>
       <h1 style={{ color: "blue", textAlign: "center" }}>{props.fullname}</h1>
-      {children=<img  src="/bio.png" alt="myphoto" />}
+      {(children = <img src="/bio.png" alt="myphoto" />)}
       <h2 style={{ color: "red", fontSize: 45 }}>
         Short biography :
         <p style={{ color: "black", fontSize: 30, textAlign: "justify" }}>
@@ -12,7 +12,7 @@ const Myprofile = (props,{children}) => {
         </p>
       </h2>
       <h2 style={{ color: "red", fontSize: 45 }}>
-        My profesion : <h3 style={{ color: "black" }}>{props.profesion}</h3>
+        My profesion : <p style={{ color: "black", fontSize:30 }}>{props.profesion}</p>
       </h2>
       <button onClick={() => props.handleName(`${props.fullname}`)}>
         ClickMe
@@ -21,7 +21,11 @@ const Myprofile = (props,{children}) => {
   );
 };
 export default Myprofile;
-Myprofile.defaultProps = ["none", "profile", "bio"];
+Myprofile.defaultProps = {
+  fullname: "none",
+  bio: "none",
+  profesion: "none",
+};
 Myprofile.propTypes = {
   fullname: PropTypes.string,
   bio: PropTypes.string,
